8b52605c4ba2589d680a25cca90f3f873667344f
mineevburyat <mineev.buryat@gmail.com>
2021-12-03 01:54:32 +0800

    lesson1 on check

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+// lesson1
 const products = [
     {id: 1, title: 'Notebook', price: 2000},
     {id: 2, title: 'Mouse', price: 20},
     {id: 3, title: 'Keyboard', price: 200},
     {id: 4, title: 'Gamepad', price: 50},
+    {id: 5, title: 'AVR', price: 200},
+    {id: 6, title: 'Speacker', price: 50},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение

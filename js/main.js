1371f175aaee3922ea892fa974f402f53f8e37d6
mineevburyat <mineev.buryat@gmail.com>
2021-12-06 23:41:38 +0800

    lesson3 base

--- js/main.js
+++ js/main.js
@@ -1,136 +1,81 @@
-// lesson2
-"use strict";
-// Класс описывает товар: название, цена, артикул, картинка(и)
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     // window.ActiveXObject -> xhr = new ActiveXObject()
+//     xhr.open("GET", url, true);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status !== 200){
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
+class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];//массив товаров из JSON документа
+        this._getProducts()
+            .then(data => { //data - объект js
+                 this.goods = data;
+                 this.render()
+            });
+    }
+    // _fetchProducts(cb){
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this.goods = JSON.parse(data);
+    //         console.log(this.goods);
+    //         cb();
+    //     })
+    // }
+    _getProducts(){
+      
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+       
+    }
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+//            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+
+    }
+}
+
+
 class ProductItem {
-    constructor (title, price, img='140x100.png') {
-        this.title = title;
-        this.price = price;
+    constructor(product, img = 'https://via.placeholder.com/200x150'){
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
         this.img = img;
     }
-    // Метод отображения карточки товара
-    render() {
-        return `<div class="product-item">
-        <h3>${this.title}</h3>
-        <img src="img/${this.img}" alt="placeholder">
-        <p class="price">${this.price}</p>
-        <button class="buy-btn">Купить</button>
-    </div>`
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
     }
 }
 
-// Список товаров, с методом получения данных со стороннего источника и формируем список товаров
-// class ProductsList {
-//     constructor () {
-//         this.goods = []
-//     }
-//     // Получение списка товаров из стороннего источника
-//     fetchGoods() {
-//         const products = [
-//             {id: 1, title: 'Notebook', price: 2000},
-//             {id: 2, title: 'Mouse', price: 20},
-//             {id: 3, title: 'Keyboard', price: 200},
-//             {id: 4, title: 'Gamepad', price: 50},
-//             {id: 5, title: 'AVR', price: 200},
-//             {id: 6, title: 'Speacker', price: 50},
-//         ];
-//         let arr = []
-//         products.forEach(function(item) {
-//             // console.log(item);
-//             let itemProduct = new ProductItem(item.title, item.price);
-//             // console.log(itemProduct);
-//             arr.push(itemProduct);
-//         });
-//         // console.log(arr);
-//         this.goods = arr;
-//     }
-//     // Формирование списка карточек товара в блоке с классом .products
-//     render() {
-//         let htmlstr = '';
-//         this.goods.forEach(function(item) {
-//             htmlstr += item.render()
-//         })
-//         document.querySelector('.products').innerHTML = htmlstr;
-//     }
-// }
-// Наследуем свойства от объекта массивы
-class ProductsList extends Array {
-    constructor () {
-        super()
-    }
-    //заполнение массива товаров
-    fetchGoods() {
-        const products = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-            {id: 5, title: 'AVR', price: 200},
-            {id: 6, title: 'Speacker', price: 50},
-        ];
-        products.forEach((item) => {
-            super.push(new ProductItem(item.title, item.price));
-        });
-    }
-    // Формирование списка карточек товара в блоке с классом .products
-    render() {
-        super.forEach((item) => {
-            document.querySelector('.products').insertAdjacentHTML('beforeend', item.render())
-        })
-    }
-    //общая стоимость товара не имеет смысла, если мы не знаем сколько товаров на складе - перенести эту функцию в корзину
-}
-//Товар в корзине это ProductItem с дополнительным параметром количество однотипных товаров
-class BasketGood extends ProductItem {
-    constructor(numbergoods=1, goods) {
-        let {title, price, img} = goods;
-        super(title, price, img);
-        this.numbergoods = numbergoods;
-    }
-    //всего на сумму данного товара в корзине
-    totalSum() {
-        return(this.numbergoods * this.price)
-    }
-    //представить товар в виде html карточки
-    render() {};
-}
-//Корзину лучше представить как массив корзинных тваров c дополнительным параметром - id пользователя
-//TODO  сделать корзину пока как отдельный класс, т.к. не понятно как работать с user_id и особенностями добавления и удаления товаров в корзине.
-class Basket {
-    constructor() {
-        this.goods = [];
-        //корзина для одного пользователя пока
-        //this.id = user_id;
-    }
-    //общая стоимость товаров в корзине
-    totalprice() {
-        let summ = 0;
-        this.goods.forEach((item) => {
-            summ += item.totalSum();
-        })
-        return summ;
-    }
-    addGoods(goods) {
-        this.goods.push(goods);
-    }
-    removeGoods(goods) {}
-    //отрисовать карточки товары в блоке корзины
-    render() {};
-}
+let list = new ProductsList();
+console.log(list.allProducts);
 
-
-//основная логика
-// создать список товаров и заполнить его
-let goodList = new ProductsList;
-goodList.fetchGoods();
-// отобразить список товаров в блоке .products
-goodList.render();
-
-const user1_basket = new Basket;
-// console.log(goodList[1], goodList[2]);
-user1_basket.addGoods(new BasketGood(2, goodList[2]));
-user1_basket.addGoods(new BasketGood(3, goodList[0]));
-console.log('В корзине следующие товары:')
-user1_basket.goods.forEach((item) => {
-    console.log(`${item.title} с ценой ${item.price} и количество таких товаров ${item.numbergoods} на общую сумму ${item.totalSum()}`);
-})
-console.log(`Всего товаров на сумму: ${user1_basket.totalprice()}`);

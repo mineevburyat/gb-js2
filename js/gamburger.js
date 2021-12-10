4e4cb210567b74921567941a754d83db7d047a89
mineevburyat <mineev.buryat@gmail.com>
2021-12-10 21:40:35 +0800

    выполнено все в классах, в html основная логика в js описание классов и методов

--- js/gamburger.js
+++ js/gamburger.js
@@ -1,40 +1,40 @@
-class paramObj {
+class ParamObj {
   constructor(element) {
     this.value = element.value;
     this.price = +element.dataset.price;
     this.calories = +element.dataset.calories;
   }
 }
-//функция возвращающая объект с выбранной опцией 
-function getSelectOptions(id_select) {
-  let returnitem;
-  [... document.getElementById(id_select).options].forEach((item) => {
-    if (item.selected) {
-    returnitem = item;
-    }
-  });
-  let obj = new paramObj(returnitem);
-  return obj;
-}
-//функция возвращает массив объектов
-function getSelectCheckbox(name_input) {
-  let arr = [... document.querySelectorAll(`input[name=${name_input}]:checked`)]
-  let arr_obj = arr.map((item) => {
-    let obj = new paramObj(item);
-    return obj;
-  });
-return arr_obj;
-}
-
 
 class Hamburger {
-  constructor(size, stuffing, topping = []) { 
-    this.size = size; //obj {value: "", text: "", price: num, calories: num}
-    this.stuffing = stuffing; //obj {value: "", text: "", price: num, calories: num}
-    this.topping = topping; //array of obj 
-   }
-  
-  getToppingsStrList() {
+  //size (str) - id of select element for size 
+  //stuffing (str) - id of select element of stuffing
+  //topping (str) - name of checkboxes 
+  constructor(size, stuffing, topping) { 
+    this.size = this._getSelectOptions(size); //obj {value: "", text: "", price: num, calories: num}
+    this.stuffing = this._getSelectOptions(stuffing); //obj {value: "", text: "", price: num, calories: num}
+    this.topping = this._getSelectCheckbox(topping); //array of obj 
+  }
+  //метод возвращающая объект (value, price, calories) с выбранной опцией 
+  _getSelectOptions(id_select) {
+    let returnitem;
+    [... document.getElementById(id_select).options].forEach((item) => {
+    if (item.selected) {
+      returnitem = item;
+      }
+    });
+    return new ParamObj(returnitem);
+  }
+  //метод возвращает массив объектов
+  _getSelectCheckbox(name_input) {
+    let arr = [... document.querySelectorAll(`input[name=${name_input}]:checked`)]
+    let arr_obj = arr.map((item) => {
+      let obj = new ParamObj(item);
+      return obj;
+    });
+    return arr_obj;
+  }
+  _getToppingsStrList() {
     let str = '';
     if (this.topping.length == 0) {
       str = "без соусов";
@@ -46,7 +46,6 @@ class Hamburger {
     }) }
     return str;
   }
-  
   calculatePrice() {
     let summ = this.size.price + this.stuffing.price;
     this.topping.forEach((item) => {
@@ -54,7 +53,6 @@ class Hamburger {
     })
     return summ;
   }
-
   calculateCalories() {
     let summ = this.size.calories + this.stuffing.calories;;
     this.topping.forEach((item) => {
@@ -62,22 +60,29 @@ class Hamburger {
     })
     return summ;
   }
-
+  
   info() {
-    return `${this.size.value} гамбургер  содержит ${this.stuffing.value}, ${this.getToppingsStrList()}`;
+    return `${this.size.value} гамбургер  содержит ${this.stuffing.value}, ${this._getToppingsStrList()}`;
+  }
+  //метод изменения тектового содержания элемента с id_element 
+  //id_element (str)- id HTML элемента
+  //value (num or str) - подставляемое значение
+  textInHTMLItem(id_element, value) {
+    const item = document.getElementById(id_element);
+    item.textContent = value;
   }
 
-  infoHTML(id_info) {
-    const info = document.getElementById(id_info);
-    info.textContent = this.info();
-  }
-  totalHTML(id_totalelement, id_calloryelement) {
-    const total = document.getElementById(id_totalelement);
-    // console.log(total);
-    const callory = document.getElementById(id_calloryelement);
-    total.textContent = this.calculatePrice();
-    callory.textContent = this.calculateCalories();
-  }
+  // infoHTML(id_info) {
+  //   const info = document.getElementById(id_info);
+  //   info.textContent = this.info();
+  // }
+  // totalHTML(id_totalelement, id_calloryelement) {
+  //   const total = document.getElementById(id_totalelement);
+  //   // console.log(total);
+  //   const callory = document.getElementById(id_calloryelement);
+  //   total.textContent = this.calculatePrice();
+  //   callory.textContent = this.calculateCalories();
+  // }
 }
 
 // const bigmac = new Hamburger('big', 'withpotatos');
